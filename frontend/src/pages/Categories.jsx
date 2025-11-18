83d1927f1bf081b9b27bfb443fd26a8205258bd9
Rron Dedushi <rrondedushi@gmail.com>
2025-11-18 15:04:30 -0500

    Fetch categories from backend API

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -4,3 +4,9 @@ import API from "../hooks/useApi";
 export default function Categories() {
   const [categories, setCategories] = useState([]);
 }
+
+useEffect(() => {
+  API.get("/categories")
+    .then((res) => setCategories(res.data))
+    .catch((err) => console.log(err));
+}, []);
