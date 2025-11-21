8eecd41c2e702d07bde063d3e42e200a57cceef3
Rron Dedushi <rrondedushi@gmail.com>
2025-11-21 10:22:59 -0500

    Add API route to get an individual category by ID

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -3,10 +3,24 @@ import { Category } from "../models/Category.js";
 
 const router = express.Router();
 
+// GET all categories //
 router.get("/", async (req, res) => {
   try {
     const categories = await Category.find().sort({ name: 1 });
     res.json(categories);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// GET -single- category by ID //
+router.get("/:id", async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+    res.json(category);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
