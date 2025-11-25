faa3a2473f51a9671a8f2aedbe154eb2cad060db
Rron Dedushi <rrondedushi@gmail.com>
2025-11-24 19:38:19 -0500

    Update App routes and import structure for new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./pages/Home";
 import Categories from "./pages/Categories";
 import StepGenerator from "./pages/StepGenerator";
 import About from "./pages/About";
+import AddCategory from "./pages/AddCategory";
+import ManageSteps from "./pages/ManageSteps";
 
 export default function App() {
   return (
@@ -15,7 +17,13 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/category/:categoryId" element={<StepGenerator />} />
+        <Route
+          path="/category/:categoryId/manage"
+          element={<ManageSteps />}
+        />{" "}
         <Route path="/about" element={<About />} />
+        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="/manage-steps/:categoryId" element={<ManageSteps />} />
       </Routes>
     </>
   );
